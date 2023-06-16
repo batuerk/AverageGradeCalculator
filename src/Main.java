9a19f55a85d3092148f96a22f26bc98133436dac
batuhan.erkul <baerkul@hotmail.com>
2023-06-16 11:33:34 +0300

    AverageGradeCalculator

--- src/Main.java
+++ src/Main.java
@@ -3,30 +3,27 @@ import java.util.Scanner;
 import java.util.List;
 public class Main {
     static List<Integer> grades = new ArrayList<>();
-    static int a,b,c,d,e;
+    static int a,b,c,d,e,avg;
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter Physic Grade: ");
+        System.out.print("Enter Physic Grade: ");
         a = input.nextInt();
         grades.add(a);
 
-        System.out.println("Enter Math Grade: ");
+        System.out.print("Enter Math Grade: ");
         b = input.nextInt();
         grades.add(b);
 
-
-        System.out.println("Enter Chemical Grade: ");
+        System.out.print("Enter Chemical Grade: ");
         c = input.nextInt();
         grades.add(c);
 
-
-        System.out.println("Enter Music Grade: ");
+        System.out.print("Enter Music Grade: ");
         d = input.nextInt();
         grades.add(d);
 
-
-        System.out.println("Enter History Grade: ");
+        System.out.print("Enter History Grade: ");
         e = input.nextInt();
         grades.add(e);
 
@@ -38,6 +35,9 @@ public class Main {
         System.out.println("Music Grade: " + d);
         System.out.println("History Grade: " + e);
 
+        avg = (a+b+c+d+e)/grades.size();
+        boolean gectiMi = (avg > 50);
+        System.out.println("Geçti mi?: " + gectiMi);
 
     }
     public static void calculate(){
